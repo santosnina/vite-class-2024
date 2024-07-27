12d2cd0a016476326c5949580dd2fa846df1d1d4
santosnina <yuwenpan0531@gmail.com>
2024-07-27 21:54:36 +0800

    Update code

--- vite-Class2/src/App.vue
+++ vite-Class2/src/App.vue
@@ -1,20 +1,16 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from "vue";
+const num = ref(2);
+function clickMe() {
+  num.value++
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    {{ num }}
+    <button type="button" v-on:click="clickMe">Click Me</button>
+  </div>
 </template>
 
 <style scoped>
